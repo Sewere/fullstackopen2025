9c3c30c28f55c769269bdac5b1f9439adc13742e
Sewere <39487964+Sewere@users.noreply.github.com>
2025-03-18 22:33:27 +0200

    part 1 done

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,66 @@
+import { use, useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  //var voteList = new Uint8Array(8);
+  const votes = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0 };
+
+  const [voteCount, setVoteCount] = useState(votes);
+  const [selected, setSelected] = useState(0);
+  const [highest, setHighest] = useState(0);
+
+  const reroll = () => {
+    const nro = getRandomInt(0, anecdotes.length - 1);
+    console.log(nro);
+    setSelected(nro);
+  };
+
+  const vote = () => {
+    const copy = { ...voteCount };
+    copy[selected] += 1;
+    setVoteCount(copy);
+    highscore(copy);
+  };
+
+  const highscore = (copyList) => {
+    let who = 0;
+    let amount = 0;
+    for (const key in copyList) {
+      console.log(`Key: ${key}, Value: ${copyList[key]}`);
+      if (copyList[key] > amount) {
+        who = key;
+        amount = copyList[key];
+      }
+    }
+    setHighest(who);
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the day: {selected}</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>Anecdote has {voteCount[selected]} votes</p>
+      <button onClick={vote}>VOTE</button>
+      <button onClick={reroll}>reroll anecdote</button>
+      <h1>Anecdote with the most votes</h1>
+      <p>{anecdotes[highest]}</p>
+      <p>Anecdote has {voteCount[highest]} votes</p>
+    </div>
+  );
+};
+
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export default App;
--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -1,33 +1,44 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header data={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
 
 const Content = (props) => (
   <div>
-    <Part name={props.parts[0].name} number={props.parts[0].exercises} />
-    <Part name={props.parts[1].name} number={props.parts[1].exercises} />
-    <Part name={props.parts[2].name} number={props.parts[2].exercises} />
+    <Part
+      name={props.course.parts[0].name}
+      number={props.course.parts[0].exercises}
+    />
+    <Part
+      name={props.course.parts[1].name}
+      number={props.course.parts[1].exercises}
+    />
+    <Part
+      name={props.course.parts[2].name}
+      number={props.course.parts[2].exercises}
+    />
   </div>
 );
 
@@ -39,12 +50,12 @@ const Part = (props) => (
 
 function Total(props) {
   const number =
-    props.parts[0].exercises +
-    props.parts[1].exercises +
-    props.parts[2].exercises;
+    props.course.parts[0].exercises +
+    props.course.parts[1].exercises +
+    props.course.parts[2].exercises;
   return <p>Number of exercises {number}</p>;
 }
 
-const Header = (props) => <h1>{props.data}</h1>;
+const Header = (props) => <h1>{props.course.name}</h1>;
 
 export default App;
